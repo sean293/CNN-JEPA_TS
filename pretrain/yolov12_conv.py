e4240da72936e00881b5bc128e0785460732e6c0
Sean Hamilton <sean.hamilton@login12.ufhpc>
2025-10-30 07:57:42 -0400

    Add yolov12 blocks

---
+++ pretrain/yolov12_conv.py
@@ -0,0 +1,28 @@
+# https://github.com/sunsmarterjie/yolov12
+
+import math
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+#NOTE From https://github.com/sunsmarterjie/yolov12/blob/main/ultralytics/nn/modules/conv.py
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        """Apply convolution and activation without batch normalization."""
+        return self.act(self.conv(x))
